4a5efb4adc4777447b0cc4a17abbe309b2e6df7d
Amine <elwafiamine78@gmail.com>
2026-01-21 02:13:23 +0100

    second section

--- public/js/app.js
+++ public/js/app.js
@@ -1,10 +1,16 @@
 const open = document.getElementById('open')
+const open1 = document.getElementById('open1')
 const close = document.getElementById('close')
 const modalContainer = document.getElementById('modalContainer')
 
 open.addEventListener('click', () => {
     modalContainer.classList.add('show');
 })
+
+open1.addEventListener('click', () => {
+    modalContainer.classList.add('show');
+})
+
 close.addEventListener('click', () => {
     modalContainer.classList.remove('show');
 })
